c0b9c5dd5e1ed9730b36f456b263fe386a739303
roubousafa <roubousafa1@gmail.com>
2025-11-27 23:15:03 +0300

    1

--- Services/IRobotService.cs
+++ Services/IRobotService.cs
@@ -4,9 +4,9 @@ namespace AdventOfCodeSolution.Services
 {
     public interface IRobotService
     {
-        // دالة لمحاكاة حركة الروبوتات
+        
         SimulationResult SimulateRobots(List<Robot> robots, int width, int height, int seconds);
-        // دالة لتحليل النص المدخل إلى كائنات روبوت
+        
         List<Robot> ParseInput(string input);
     }
 }
